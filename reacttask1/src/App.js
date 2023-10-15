cad134c0d669902a719b9ecffe444d41daccdb63
arshad1151 <mohamed.arshad2003@gmail.com>
2023-10-15 14:23:02 +0530

    first commit

---
+++ reacttask1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import WordCounter from './components/WordCount';
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <WordCounter/> 
+    </div>
+  );
+}
+export default App;
